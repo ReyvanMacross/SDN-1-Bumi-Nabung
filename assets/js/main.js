7b51a4c1a04023178150474696a0b4a1a60be1e7
unknown <rievanrivaldynurtianax@gmail.com>
2024-12-09 21:27:50 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    const owl = $("#slider-hero");
+
+    owl.owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      nav: false, 
+      dots: true,
+    });
+  
+    $(".slider-prev").click(function () {
+      owl.trigger("prev.owl.carousel");
+    });
+  
+    $(".slider-next").click(function () {
+      owl.trigger("next.owl.carousel");
+    });
+
+    $('#tenaga-pendidik-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        // mouseDrag: false,
+        items: 3,
+        dots: false,
+        margin: 20,
+        navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        navContainer: '#slider-tools-1',
+});
+
+    $('#alumni-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        // mouseDrag: false,
+        items: 2,
+        dots: false,
+        margin: 20,
+        navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        navContainer: '#slider-tools-2',
+});
+
+$('#galeri-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    // mouseDrag: false,
+    items: 3,
+    dots: false,
+    margin: 20,
+    navText: [
+    '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+    '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    navContainer: '#slider-tools-3',
+});
+
+});
